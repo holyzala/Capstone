79539460bb87ee6e1936e569c4d51b42d5fd37fb
Musaed Alsuwaidan <musaed919@gmail.com>
2018-01-31 16:04:32 -0600

    Models folder and some classes

---
+++ client/sheepshead/sheepshead/Resources/models/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace sheepshead.Resources.models
+{
+    class Card : ICard
+    {
+        public Card(){}
+        public Card(int num, int value, int power, Suit cardSuit)
+        {
+            this.Num = num;
+            this.Value = value;
+            this.Power = power;
+            this.CardSuit = CardSuit;
+        }
+
+        public int Num { get ; set ; }
+        public int Value { get ; set ; }
+        public int Power { get ; set ; }
+        public Suit CardSuit { get ; set ; }
+
+        public bool isHigher(ICard otherCard)
+        {
+            return this.Power > otherCard.Power;
+        }
+
+        public bool isTrump()
+        {
+            return this.CardSuit == Suit.Trump;
+        }
+    }
+}
---
+++ client/sheepshead/sheepshead/Resources/models/Hand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace sheepshead.Resources.models
+{
+    class Hand
+    {
+        private Card[] cards = new Card[6];
+
+        public Hand() { }
+
+
+
+        public int getNumOfRemainingCards()
+        {
+            int size = 0;
+            for (int i=0; i < cards.Length; i++)
+            {
+                if(cards[i] != null)  size++;
+            }
+            return size;
+        }
+
+
+
+        public Card getCard(int index)
+        {
+            return cards[index];
+        }
+
+
+    }
+}
