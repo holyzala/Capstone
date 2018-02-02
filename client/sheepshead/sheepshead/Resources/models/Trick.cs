95e38f749d6ba2448f733bed7f343617fd76cb50
Musaed Alsuwaidan <musaed919@gmail.com>
2018-02-01 23:10:10 -0600

    Trick class

---
+++ client/sheepshead/sheepshead/Resources/models/Trick.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using iface = sheepshead.Resources.models.interfaces;
+
+
+namespace sheepshead.Resources.models
+{
+    class Trick
+    {
+        List<(iface.IPlayer,iface.ICard)> TrickCards { get; set; }
+        iface.IPlayer TrickWinner { get; set; }
+
+
+        public (iface.IPlayer, iface.ICard) CurrentWinningCard()
+        {
+            (iface.IPlayer, iface.ICard) winner = (null,null);
+         
+            if(TrickCards.Count != 0)
+            {
+                for(int i =0; i< TrickCards.Count; i++)
+                {
+                    if (TrickCards[i].Item2.IsHigher(winner.Item2))
+                        winner = TrickCards[i];
+                }
+            }
+            return winner;
+        }
+
+    }
+}
