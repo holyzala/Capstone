009a2549695c7b3c37c6ee815cab2410bd01460e
Musaed Alsuwaidan <musaed919@gmail.com>
2018-02-02 22:36:19 -0600

    IBlind, Blind, ITrick, Trick, TrckTest, and LocalPlayer were added.

--- client/sheepshead/sheepshead/Resources/models/Trick.cs
+++ client/sheepshead/sheepshead/Resources/models/Trick.cs
@@ -15,26 +15,57 @@ using iface = sheepshead.Resources.models.interfaces;
 
 namespace sheepshead.Resources.models
 {
-    class Trick
+    public class Trick : iface.ITrick
     {
-        List<(iface.IPlayer,iface.ICard)> TrickCards { get; set; }
-        iface.IPlayer TrickWinner { get; set; }
+
+        public List<(iface.IPlayer, iface.ICard)> TrickCards { get; private set ; }
 
 
-        public (iface.IPlayer, iface.ICard) CurrentWinningCard()
+
+        public Trick(List<(iface.IPlayer, iface.ICard)> cards)
         {
-            (iface.IPlayer, iface.ICard) winner = (null,null);
-         
-            if(TrickCards.Count != 0)
+            TrickCards = new List<(iface.IPlayer, iface.ICard)>(5);
+            TrickCards = cards;
+        }
+
+
+
+        public iface.ICard LeadingCard()
+        {
+            return TrickCards[0].Item2;
+        }
+        public iface.IPlayer TheWinnerPlayer()
+        {
+            iface.IPlayer winner = TrickCards[0].Item1;
+            for (int i=0; i<TrickCards.Count;i++)
             {
-                for(int i =0; i< TrickCards.Count; i++)
-                {
-                    if (TrickCards[i].Item2.IsHigher(winner.Item2))
-                        winner = TrickCards[i];
-                }
+                iface.ICard card = TrickCards[i].Item2;
+                if (TheWinnerCard().Equals(card))
+                    winner = TrickCards[i].Item1;
+            } 
+            return winner;
+        }
+
+
+
+
+        public iface.ICard TheWinnerCard()
+        {
+            iface.ICard winner = LeadingCard();
+            foreach((iface.IPlayer,iface.ICard) crd in TrickCards)
+            {
+                iface.ICard card = crd.Item2;
+                if (winner.CardSuit == card.CardSuit && winner.Power < card.Power)
+                    winner = card;
+                else if (winner.CardSuit != Suit.Trump && card.CardSuit == Suit.Trump)
+                    winner = card;
             }
             return winner;
         }
 
+
+
     }
 }
+
+
---
+++ client/sheepshead/sheepsheadTests/Resources/models/TrickTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sheepshead.Resources.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace sheepshead.Resources.models.Tests
+{
+    [TestClass()]
+    public class TrickTests
+    {
+        [TestMethod()]
+        public void TheWinnerCardTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
