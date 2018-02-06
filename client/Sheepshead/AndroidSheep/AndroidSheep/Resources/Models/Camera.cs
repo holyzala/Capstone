13ad8ba1fe3cfc01c90123ab5d79c7412378fd01
Strygyr <dorsey2@uwm.edu>
2018-02-05 20:52:09 -0600

    Added Camera and a Ground Tile for test purpsoses

---
+++ client/Sheepshead/AndroidSheep/AndroidSheep/Resources/Models/Camera.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using iface = SH_Android.Resources.Models.Interfaces;
+
+namespace SH_Android.Resources.Models
+{
+    class Camera : iface.ICamera
+    {
+        GraphicsDevice graphicsDevice;
+
+        public Camera(string name, Vector3 startPos)
+        {
+
+            this.Position = startPos;
+            this.Name = name;
+            this.FieldOfView = Microsoft.Xna.Framework.MathHelper.PiOver4;
+            this.AspectRatio = (float)graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
+            this.NearPlane = 1;
+            this.FarPlane = 200;
+        }
+
+        public bool IsLocked { get; private set; }
+        public string Name { get; private set; }
+        public Vector3 Position { get; private set; }
+
+        public float FieldOfView { get; private set; }
+        public float NearPlane { get; private set; }
+        public float FarPlane { get; private set; }
+        public float AspectRatio { get; private set; }
+
+        public Matrix PerspectiveMatrix
+        {
+            get
+            {
+                return Matrix.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearPlane, FarPlane);
+            }
+        }
+
+        public Matrix ViewMatrix
+        {
+            get
+            {
+                var lookAtVector = Vector3.Zero;
+                var upVector = Vector3.UnitZ;
+                return Matrix.CreateLookAt(Position, lookAtVector, upVector);
+            }
+        }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // We'll be doing some input-based movement here
+        }
+    }
+}
---
+++ client/Sheepshead/AndroidSheep/AndroidSheep/Resources/Models/Interfaces/ICamera.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SH_Android.Resources.Models.Interfaces
+{
+    interface ICamera
+    {
+        string Name { get; }
+        Vector3 Position { get; }
+
+        float FieldOfView { get; }
+        float NearPlane { get; }
+        float FarPlane { get; }
+        float AspectRatio { get; }
+
+        bool IsLocked { get; }
+        Matrix PerspectiveMatrix { get; }
+        Matrix ViewMatrix { get; }
+
+        /* TO DO:
+         *
+         * ToString()
+         * == Method
+         * != Method
+         * Possibly .Equals for whatever reason
+         */
+        /* Possible TO DO:
+         * Vector3 Translation - Moving the Camera
+         * Vector3 Rotation - Rotating the Camera
+         * Vector3 Scaling - Zooming in and Out
+         */
+
+    }
+}
