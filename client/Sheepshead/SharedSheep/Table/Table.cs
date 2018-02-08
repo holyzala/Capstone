b7a9daebbc0b31e36cad5de1557b1a8ed9def1c8
Musaed Alsuwaidan <musaed919@gmail.com>
2018-02-08 17:14:20 -0600

    ITable, Table. and small fixes

---
+++ client/Sheepshead/SharedSheep/Table/Table.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedSheep.Game;
+using SharedSheep.Player;
+
+namespace SharedSheep.Table
+{
+    class Table : ITable
+    {
+        public List<IPlayer> Players { get; private set; }
+
+        public List<IGame> Games { get; private set; }
+
+        public IPlayer Dealer { get; private set; }
+
+
+        public Table()
+        {
+            Players = new List<IPlayer>();
+            Games = new List<IGame>();
+            //Dealer = new IPlayer(); this is going to be the host
+         }
+
+        public void AddPlayer(IPlayer player)
+        {
+            Players.Add(player);
+        }
+
+        public IPlayer GetNextPlayer(int CurrentPlayerIndexNumber)
+        {
+            return Players[(CurrentPlayerIndexNumber + 1)%5];
+        }
+
+        public void ResetGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartNewGame()
+        {            
+            Game.Game game = new Game.Game();
+            Games.Add(game);
+            game.StartGame(Players);
+        }
+    }
+}
