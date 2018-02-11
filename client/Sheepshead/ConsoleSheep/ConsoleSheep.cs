efa3b1915ed18a8673ffe2842c8d5af3c35605d6
Christopher Magnuson <magnus32@uwm.edu>
2018-02-11 15:57:45 -0600

    Skeleton of the text console.

---
+++ client/Sheepshead/ConsoleSheep/ConsoleSheep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SharedSheep.Table;
+
+namespace ConsoleSheep
+{
+    class ConsoleSheep
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            ITable table = new Table(Prompt);
+        }
+
+        static string Prompt(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+    }
+}
