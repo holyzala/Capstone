97a0b93d17742445e85abad2d6bd4c3707959ce9
Strygyr <dorsey2@uwm.edu>
2018-02-26 19:27:55 -0600

    Updated table graphics, debug camera updated, creating player instances and card instances

--- client/Sheepshead/AndroidSheep/Models/Camera/PlayerCamera.cs
+++ client/Sheepshead/AndroidSheep/Models/Camera/PlayerCamera.cs
@@ -1,33 +1,35 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+using System.Diagnostics;
 
 namespace AndroidSheep.Models
 {
-    internal class PlayerCamera : ICamera
+    class PlayerCamera : ICamera
     {
-
-        public PlayerCamera(string name, Vector3 startPos, GraphicsDeviceManager graphics)
+        public PlayerCamera(Vector3 startpos, GraphicsDeviceManager graphics)
         {
-            this.Position = startPos;
-            this.Name = name;
+            this.Position = startpos;
             this.Graphics = graphics.GraphicsDevice;
             this.FieldOfView = Microsoft.Xna.Framework.MathHelper.PiOver4;
             this.AspectRatio = (float)Graphics.Viewport.Width / (float)Graphics.Viewport.Height;
             this.NearPlane = 1;
             this.FarPlane = 200;
+            this.LookAt = new Vector3(0, -1, -5f);
+            this.UpVector = new Vector3(0, 0, 1f);
         }
 
-        public bool IsLocked { get; private set; }
-        public string Name { get; private set; }
-        public Vector3 Position { get; private set; }
-
+        public Vector3 Position { get; set; }
+        public Vector3 UpVector { get; private set; }
+        public Vector3 LookAt { get; private set; }
         public GraphicsDevice Graphics { get; private set; }
+
         public float FieldOfView { get; private set; }
         public float NearPlane { get; private set; }
         public float FarPlane { get; private set; }
         public float AspectRatio { get; private set; }
 
-        public Matrix PerspectiveMatrix
+        public Matrix ProjectionMatrix
         {
             get
             {
@@ -39,15 +41,23 @@ namespace AndroidSheep.Models
         {
             get
             {
-                var lookAtVector = Vector3.Zero;
+                var lookAtVector = new Vector3(0, -10.0f, -0.5f);
+                // We'll create a rotation matrix using our angle
+                var rotationMatrix = Matrix.CreateRotationX(0.66f);
+                // Then we'll modify the vector using this matrix:
+                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
+                lookAtVector += this.Position;
+
                 var upVector = Vector3.UnitZ;
-                return Matrix.CreateLookAt(Position, lookAtVector, upVector);
+
+                return Matrix.CreateLookAt(
+                    Position, lookAtVector, upVector);
             }
         }
 
         public void Update(GameTime gameTime)
         {
-            // We'll be doing some input-based movement here
+
         }
     }
 }
